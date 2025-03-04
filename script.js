132053d73d5d8d396cdb74103967e043513b111b
qecide <134002609+qecide@users.noreply.github.com>
2025-03-04 21:56:32 +0300

    добавление мониторинга событий для ga

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// Отслеживание ввода чисел
+document.querySelectorAll("#value1, #value2").forEach(input => {
+    input.addEventListener("input", function() {
+        gtag('event', 'number_input', {
+            'event_category': 'Калькулятор',
+            'event_label': this.id,
+            'value': this.value
+        });
+    });
+});
+
+// Отслеживание выбора математической операции
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener("click", function() {
+        gtag('event', 'operation_selected', {
+            'event_category': 'Калькулятор',
+            'event_label': this.innerText
+        });
+    });
+});
+
+// Отправка события при расчете результата
 function calculate(operation) {
     var value1 = document.getElementById('value1').value;
     var value2 = document.getElementById('value2').value;
@@ -9,56 +31,27 @@ function calculate(operation) {
 
     value1 = parseFloat(value1);
     value2 = parseFloat(value2);
-
     var result;
-    switch(operation) {
-        case '+':
-            result = value1 + value2;
-            break;
-        case '-':
-            result = value1 - value2;
-            break;
-        case '*':
-            result = value1 * value2;
-            break;
-        case '/':
-            if (value2 === 0) {
-                alert("Не делай так");
-                return;
-            }
-            result = value1 / value2;
-            break;
-        case 'sqrt':
-            result = Math.sqrt(value1);
-            break;
-        case 'pow':
-            result = Math.pow(value1, value2);
-            break;
-        case 'mod':
-            result = Math.abs(value1);
-            break;
-        default:
-            result = "Чего?";
+
+    switch (operation) {
+        case '+': result = value1 + value2; break;
+        case '-': result = value1 - value2; break;
+        case '*': result = value1 * value2; break;
+        case '/': result = value2 === 0 ? "Ошибка" : value1 / value2; break;
+        case 'sqrt': result = Math.sqrt(value1); break;
+        case 'pow': result = Math.pow(value1, value2); break;
+        case 'mod': result = Math.abs(value1); break;
+        default: result = "Ошибка";
     }
 
     document.getElementById('result').value = result;
 
-    // ОТПРАВКА СОБЫТИЯ В GOOGLE ANALYTICS
-    gtag('event', 'button_click', {
+    // Отправка события "result_calculated" в GA4
+    gtag('event', 'result_calculated', {
         'event_category': 'Калькулятор',
-        'event_label': operation,
+        'operation': operation,
         'value1': value1,
-        'value2': value2
+        'value2': value2,
+        'result': result
     });
 }
-
-// ДОБАВЛЕНИЕ ОТСЛЕЖИВАНИЯ СОБЫТИЙ ДЛЯ ВСЕХ КНОПОК
-
-document.querySelectorAll("button").forEach(button => {
-    button.addEventListener("click", function() {
-        gtag('event', 'button_click', {
-            'event_category': 'Калькулятор',
-            'event_label': this.innerText
-        });
-    });
-});
